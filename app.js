f66703d5ee257a6766e734a611d4e4e93ada3ed1
liuzhen88 <1458861320@qq.com>
2018-04-14 22:37:42 +0800

    test

--- app.js
+++ app.js
@@ -17,6 +17,11 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 var pool = new sql.ConnectionPool({
@@ -30,11 +35,7 @@ var pool = new sql.ConnectionPool({
 	}else{
 		console.log('db connect success');
 
-		app.use(logger('dev'));
-		app.use(bodyParser.json());
-		app.use(bodyParser.urlencoded({ extended: false }));
-		app.use(cookieParser());
-		app.use(express.static(path.join(__dirname, 'public')));
+		
 
 		app.use(function(req, res, next){
 			req.db = pool.request();
--- service/quality_auth.js
+++ service/quality_auth.js
@@ -2,6 +2,19 @@
 
 module.exports = {
 	getQualityAuthData:function(req, res, next){
-		
+		req.db.query(
+			'select * from T2_3_1 limit 10,10',
+			(err, result) => {
+				if(err){
+					return next(error(500, err));
+				}else{
+					res.status(200).json({
+						isSuccess:true,
+						message:'操作成功',
+						data:result
+					})
+				}
+			}
+		)
 	}
 }
