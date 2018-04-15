ce9c583a2e467a535cb3a82e7bd1b893fc51f453
liuzhen88 <1458861320@qq.com>
2018-04-15 10:26:38 +0800

    test

--- service/quality_auth.js
+++ service/quality_auth.js
@@ -2,22 +2,32 @@ var error = require('../util/error');
 
 module.exports = {
 	getQualityAuthData:function(req, res, next){
-		req.db.query(
-			"select top 1 * from T2_3_1",
-			(err, result) => {
-				if(err){
-					console.log(err);
-					return next(error(500, err));
-				}else{
-					console.log(result);
-					res.status(200).json({
-						isSuccess:true,
-						message:'操作成功',
-						data:result
-					})
+		var cpbh = req.query.cpbh;
+		if(!cpbh){
+			res.status(200).json({
+				isSuccess:false,
+				message:'缺少产品编号'
+			})
+		}
+		else{
+			req.db.query(
+				"select * from T2_3_1 where yangpin_xh='"+cpbh+"'",
+				(err, result) => {
+					if(err){
+						console.log(err);
+						return next(error(500, err));
+					}else{
+						console.log(result);
+						res.status(200).json({
+							isSuccess:true,
+							message:'操作成功',
+							data:result.recordset
+						})
+					}
 				}
-			}
-		)
+			)
+		}
+			
 	}
 }
 
